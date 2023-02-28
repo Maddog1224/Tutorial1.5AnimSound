a29d9092a87f0d1289568eeab6ab34a16f384e0e
Maddog1224 <maddog1224@outlook.com>
2023-02-28 14:53:45 -0500

    Animation commit

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -10,10 +10,14 @@ public class NewBehaviourScript : MonoBehaviour
 
     public AudioSource musicSource;
 
+    Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+         anim = GetComponent<Animator>();
+         
     }
 
     // Update is called once per frame
@@ -23,25 +27,26 @@ public class NewBehaviourScript : MonoBehaviour
         {
           musicSource.clip = musicClipOne;
           musicSource.Play();
-
+          anim.SetInteger("State", 1);
          }
 
      if (Input.GetKeyUp(KeyCode.W))
         {
           musicSource.Stop();
-
+          anim.SetInteger("State", 0);
          }
 
      if (Input.GetKeyDown(KeyCode.R))
         {
           musicSource.clip = musicClipTwo;
           musicSource.Play();
+          anim.SetInteger("State", 2);
          }
 
      if (Input.GetKeyUp(KeyCode.R))
         {
           musicSource.Stop();
-
+         anim.SetInteger("State", 0);
          }
 
      if (Input.GetKeyDown(KeyCode.L))
